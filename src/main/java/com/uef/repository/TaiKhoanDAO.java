cd5bec3db5750f6d12187d92274b860d2be8d033
QuangMinh-kkk <quangminh94.pch.1718@gmail.com>
2025-06-12 01:33:57 +0700

    Update TaiKhoanDAO.java

--- src/main/java/com/uef/repository/TaiKhoanDAO.java
+++ src/main/java/com/uef/repository/TaiKhoanDAO.java
@@ -7,6 +7,7 @@ package com.uef.repository;
 import com.uef.model.TaiKhoan;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
@@ -31,7 +32,7 @@ public class TaiKhoanDAO implements TaiKhoanRepo {
         }
     }
 
-@Override
+    @Override
     public int save(TaiKhoan taiKhoan) {
         try {
             String sql = "INSERT INTO [Tài Khoản] (email, matKhau, hoTen, sdtDienThoai, quyenHan) VALUES (?, ?, ?, ?, ?)";
@@ -49,12 +50,5 @@ public class TaiKhoanDAO implements TaiKhoanRepo {
             // Ném lỗi có ý nghĩa
             throw new IllegalArgumentException("Email đã tồn tại. Vui lòng sử dụng email khác.");
         }
-    }
-
-    @Override
-    public boolean isEmailExists(String email) {
-        String sql = "SELECT COUNT(*) FROM [Tài Khoản] WHERE email = ?";
-        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
-        return count != null && count > 0;
     }
 }
