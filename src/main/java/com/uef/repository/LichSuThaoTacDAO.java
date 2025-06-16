1fbc69b50d3048503d565b50a6b9c1a406d0be30
QuangMinh-kkk <quangminh94.pch.1718@gmail.com>
2025-06-17 00:49:27 +0700

    Update LichSuThaoTacDAO.java

--- src/main/java/com/uef/repository/LichSuThaoTacDAO.java
+++ src/main/java/com/uef/repository/LichSuThaoTacDAO.java
@@ -1,50 +1,46 @@
 package com.uef.repository;
 
 import com.uef.model.LichSuThaoTac;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-
 @Repository
 public class LichSuThaoTacDAO implements LichSuRepo {
 
+    private final JdbcTemplate jdbcTemplate;
     private final DataSource dataSource;
 
-    public LichSuThaoTacDAO(DataSource dataSource) {
+    public LichSuThaoTacDAO(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.dataSource = dataSource;
-    }
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Override
-    public List<LichSuThaoTac> getAll() {
-        String sql = "SELECT * FROM [LichSuThaoTac] ORDER BY thoiGian DESC";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(LichSuThaoTac.class));
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public boolean save(LichSuThaoTac log) {
-        String sql = "INSERT INTO LichSuThaoTac (maTNV, hanhDong, truocKhiSua, sauKhiSua, thoiGian) VALUES (?, ?, ?, ?, GETDATE())";
-
+        String sql = "INSERT INTO LICHSUTHAOTAC (maThanhVien, hanhDong, truocKhiSua, sauKhiSua, thoiGian) "
+                + "VALUES (?, ?, ?, ?, GETDATE())";
         try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-            stmt.setInt(1, log.getMaTNV());
+            stmt.setInt(1, log.getMaThanhVien());
             stmt.setString(2, log.getHanhDong());
             stmt.setString(3, log.getTruocKhiSua());
             stmt.setString(4, log.getSauKhiSua());
-
             return stmt.executeUpdate() > 0;
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("❌ Lỗi khi ghi log lịch sử: " + e.getMessage());
             return false;
         }
+    }
+
+    @Override
+    public List<LichSuThaoTac> getAll() {
+        String sql = "SELECT * FROM LICHSUTHAOTAC ORDER BY thoiGian DESC";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(LichSuThaoTac.class));
     }
 }
