057713e3f41f46b6f149b4d09cb3d51e69d0d70a
hieuthisony <hieusony28032004@gmail.com>
2025-06-24 22:18:13 +0700

    Thêm service Duyệt đăng ký

---
+++ src/main/java/com/uef/service/DuyetDangKyService.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uef.service;
+
+import com.uef.model.DuyetDangKy;
+import com.uef.repository.DuyetDangKyRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DuyetDangKyService {
+
+    @Autowired
+    private DuyetDangKyRepository duyetDangKyRepository;
+
+    public List<DuyetDangKy> getDanhSachDangKyCanDuyet(int page, String keyword, String location, String trangThai) {
+        return duyetDangKyRepository.layDanhSachDangKyCanDuyet(page, keyword, location, trangThai);
+    }
+
+    public int getTongSoDangKyCanDuyet(String keyword, String location, String trangThai) {
+        return duyetDangKyRepository.demTongSoDangKyCanDuyet(keyword, location, trangThai);
+    }
+
+    public DuyetDangKy getDangKyCanDuyetTheoMa(int maDDK) {
+        return duyetDangKyRepository.layDangKyCanDuyetTheoMa(maDDK);
+    }
+
+    public void capNhatTrangThaiDuyet(int maDDK, String trangThaiDuyet, String ghiChu) {
+        duyetDangKyRepository.capNhatTrangThaiDuyet(maDDK, trangThaiDuyet, ghiChu);
+    }
+}
