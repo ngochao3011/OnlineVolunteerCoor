0cc5642bd5d3b6fc654f2134af6235b2f20a9e5d
QuangMinh-kkk <quangminh94.pch.1718@gmail.com>
2025-06-25 15:52:02 +0700

    Update HomeController.java

--- src/main/java/com/uef/controller/HomeController.java
+++ src/main/java/com/uef/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.uef.controller;
 
+import com.uef.model.TaiKhoan;
+import com.uef.model.Volunteer;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    private void addLoginInfoToModel(Model model, HttpSession session) {
+        TaiKhoan loggedInAccount = (TaiKhoan) session.getAttribute("loggedInAccount");
+        Volunteer loggedInProfile = (Volunteer) session.getAttribute("loggedInProfile");
+
+        model.addAttribute("loggedInAccount", loggedInAccount);
+        model.addAttribute("loggedInProfile", loggedInProfile);
+        model.addAttribute("isLoggedIn", loggedInAccount != null && loggedInProfile != null);
+    }
+
     @RequestMapping("/")
-    public String home(Model model) {
+    public String home(Model model, HttpSession session) {
         model.addAttribute("pageTitle", "Online Volunteer Coor");
         model.addAttribute("pageContent", "/WEB-INF/views/homepage.jsp");
+
+        addLoginInfoToModel(model, session); 
+
         return "layout/layoutmaster";
     }
 
@@ -26,6 +40,9 @@ public class HomeController {
     public String errorPage(HttpSession session, Model model) {
         model.addAttribute("pageTitle", "Error");
         model.addAttribute("pageContent", "/WEB-INF/views/403.jsp");
+
+        addLoginInfoToModel(model, session); 
+
         return "layout/layoutmaster";
     }
 }
