d11c460de850166ef688bbb67e2b175f63205144
NguyenNgocLinhNhi <linhnhi258ds@gmail.com>
2025-06-25 22:57:17 +0700

    update thống kê

--- src/main/java/com/uef/model/ThongKe.java
+++ src/main/java/com/uef/model/ThongKe.java
@@ -4,40 +4,90 @@
  */
 package com.uef.model;
 
+import java.sql.Date;
+
+/**
+ *
+ * @author Asus
+ */
 /**
  *
  * @author Asus
  */
 public class ThongKe {
 
-    // Thống kê hoạt động 
+    private String maHoatDong;
+    private String tenHoatDong;
+    private Date ngayBatDau;
+    private Date ngayKetThuc;
+    private String diaDiem;
+
+    // ========== THỐNG KÊ HOẠT ĐỘNG ==========
+    private String trangThaiHoatDong;
     private int tongSoHoatDong;
-    private int soHoanThanh;
-    private int soDangThucHien;
-    private int soDaHuy;
-    private double tiLeTangTruongTong;
-    private double tiLeTangTruongHoanThanh;
-    private double tiLeTangTruongDangThucHien;
-    private double tiLeTangTruongDaHuy;
+    private int soHoatDongHoanThanh;
+    private int soHoatDongDangThucHien;
+    private int soHoatDongDaHuy;
 
-    // Thống kê tình nguyện viên 
-    private int tongTNV;
-    private int tongSo;
-    private double tiLeTangTruong;
+    // ========== THỐNG KÊ THEO THỜI GIAN ==========
+    private Date tuNgay;
+    private Date denNgay;
 
-    public int getTongTNV() {
-        return tongTNV;
+    // ========== CONSTRUCTORS ==========
+    public ThongKe() {
     }
 
-    public void setTongTNV(int tongTNV) {
-        this.tongTNV = tongTNV;
+    // ========== GETTERS & SETTERS ==========
+    // Thông tin chung
+    public String getMaHoatDong() {
+        return maHoatDong;
     }
 
-    private String thang;
-    private int soLuongTNV;
+    public void setMaHoatDong(String maHoatDong) {
+        this.maHoatDong = maHoatDong;
+    }
 
-    // Getters & Setters
-    //  phần hoạt động 
+    public String getTenHoatDong() {
+        return tenHoatDong;
+    }
+
+    public void setTenHoatDong(String tenHoatDong) {
+        this.tenHoatDong = tenHoatDong;
+    }
+
+    public Date getNgayBatDau() {
+        return ngayBatDau;
+    }
+
+    public void setNgayBatDau(Date ngayBatDau) {
+        this.ngayBatDau = ngayBatDau;
+    }
+
+    public Date getNgayKetThuc() {
+        return ngayKetThuc;
+    }
+
+    public void setNgayKetThuc(Date ngayKetThuc) {
+        this.ngayKetThuc = ngayKetThuc;
+    }
+
+    public String getDiaDiem() {
+        return diaDiem;
+    }
+
+    public void setDiaDiem(String diaDiem) {
+        this.diaDiem = diaDiem;
+    }
+
+    // Thống kê hoạt động
+    public String getTrangThaiHoatDong() {
+        return trangThaiHoatDong;
+    }
+
+    public void setTrangThaiHoatDong(String trangThaiHoatDong) {
+        this.trangThaiHoatDong = trangThaiHoatDong;
+    }
+
     public int getTongSoHoatDong() {
         return tongSoHoatDong;
     }
@@ -46,29 +96,48 @@ public class ThongKe {
         this.tongSoHoatDong = tongSoHoatDong;
     }
 
-    public int getSoHoanThanh() {
-        return soHoanThanh;
+    public int getSoHoatDongHoanThanh() {
+        return soHoatDongHoanThanh;
     }
 
-    public void setSoHoanThanh(int soHoanThanh) {
-        this.soHoanThanh = soHoanThanh;
+    public void setSoHoatDongHoanThanh(int soHoatDongHoanThanh) {
+        this.soHoatDongHoanThanh = soHoatDongHoanThanh;
     }
 
-    public int getSoDangThucHien() {
-        return soDangThucHien;
+    public int getSoHoatDongDangThucHien() {
+        return soHoatDongDangThucHien;
     }
 
-    public void setSoDangThucHien(int soDangThucHien) {
-        this.soDangThucHien = soDangThucHien;
+    public void setSoHoatDongDangThucHien(int soHoatDongDangThucHien) {
+        this.soHoatDongDangThucHien = soHoatDongDangThucHien;
     }
 
-    public int getSoDaHuy() {
-        return soDaHuy;
+    public int getSoHoatDongDaHuy() {
+        return soHoatDongDaHuy;
     }
 
-    public void setSoDaHuy(int soDaHuy) {
-        this.soDaHuy = soDaHuy;
+    public void setSoHoatDongDaHuy(int soHoatDongDaHuy) {
+        this.soHoatDongDaHuy = soHoatDongDaHuy;
     }
+
+    // Thống kê theo thời gian
+    public Date getTuNgay() {
+        return tuNgay;
+    }
+
+    public void setTuNgay(Date tuNgay) {
+        this.tuNgay = tuNgay;
+    }
+
+    public Date getDenNgay() {
+        return denNgay;
+    }
+
+    public void setDenNgay(Date denNgay) {
+        this.denNgay = denNgay;
+    }
+
+    private double tiLeTangTruongTong;
 
     public double getTiLeTangTruongTong() {
         return tiLeTangTruongTong;
@@ -78,6 +147,18 @@ public class ThongKe {
         this.tiLeTangTruongTong = tiLeTangTruongTong;
     }
 
+    private int soHoanThanh;
+
+    public int getSoHoanThanh() {
+        return soHoanThanh;
+    }
+
+    public void setSoHoanThanh(int soHoanThanh) {
+        this.soHoanThanh = soHoanThanh;
+    }
+
+    private double tiLeTangTruongHoanThanh;
+
     public double getTiLeTangTruongHoanThanh() {
         return tiLeTangTruongHoanThanh;
     }
@@ -86,30 +167,55 @@ public class ThongKe {
         this.tiLeTangTruongHoanThanh = tiLeTangTruongHoanThanh;
     }
 
+    private int soDangThucHien;
+
+    public void setSoDangThucHien(int soDangThucHien) {
+        this.soDangThucHien = soDangThucHien;
+    }
+
+    public int getSoDangThucHien() {
+        return soDangThucHien;
+    }
+
+    private double tiLeTangTruongDangThucHien;
+
+    public void setTiLeTangTruongDangThucHien(double tiLeTangTruongDangThucHien) {
+        this.tiLeTangTruongDangThucHien = tiLeTangTruongDangThucHien;
+    }
+
     public double getTiLeTangTruongDangThucHien() {
         return tiLeTangTruongDangThucHien;
     }
 
-    public void setTiLeTangTruongDangThucHien(double tiLeTangTruongDangThucHien) {
-        this.tiLeTangTruongDangThucHien = tiLeTangTruongDangThucHien;
+    private int soDaHuy;
+
+    public void setSoDaHuy(int soDaHuy) {
+        this.soDaHuy = soDaHuy;
+    }
+
+    public int getSoDaHuy() {
+        return soDaHuy;
+    }
+
+    private double tiLeTangTruongDaHuy;
+
+    public void setTiLeTangTruongDaHuy(double tiLeTangTruongDaHuy) {
+        this.tiLeTangTruongDaHuy = tiLeTangTruongDaHuy;
     }
 
     public double getTiLeTangTruongDaHuy() {
         return tiLeTangTruongDaHuy;
     }
 
-    public void setTiLeTangTruongDaHuy(double tiLeTangTruongDaHuy) {
-        this.tiLeTangTruongDaHuy = tiLeTangTruongDaHuy;
-    }
-
-    //  phần TNV
-    public String getThang() {
-        return thang;
-    }
-
-    public void setThang(String thang) {
-        this.thang = thang;
-    }
+//TNV
+    private int tongTNV;
+    private int tongTNVThamGia;
+    private int soLuongTNV;
+    private int maThanhVien;
+private String hoTen;
+private String email;
+private String sdt;
+   
 
     public int getSoLuongTNV() {
         return soLuongTNV;
@@ -119,20 +225,35 @@ public class ThongKe {
         this.soLuongTNV = soLuongTNV;
     }
 
-    public int getTongSo() {
-        return tongSo;
+    public int getTongTNV() {
+        return tongTNV;
     }
 
-    public double getTiLeTangTruong() {
-        return tiLeTangTruong;
+    public void setTongTNV(int tongTNV) {
+        this.tongTNV = tongTNV;
     }
 
-    public void setTongSo(int tongSo) {
-        this.tongSo = tongSo;
+    public int getTongTNVThamGia() {
+        return tongTNVThamGia;
     }
 
-    public void setTiLeTangTruong(double tiLeTangTruong) {
-        this.tiLeTangTruong = tiLeTangTruong;
+    public void setTongTNVThamGia(int tongTNVThamGia) {
+        this.tongTNVThamGia = tongTNVThamGia;
     }
-    
+
+    @Override
+    public String toString() {
+        return "ThongKe {"
+                + "maHoatDong='" + maHoatDong + '\''
+                + ", tenHoatDong='" + tenHoatDong + '\''
+                + ", ngayBatDau=" + ngayBatDau
+                + ", ngayKetThuc=" + ngayKetThuc
+                + ", diaDiem='" + diaDiem + '\''
+                + ", trangThaiHoatDong='" + trangThaiHoatDong + '\''
+                + ", soLuongTNV=" + soLuongTNV
+                + ", tongTNV=" + tongTNV
+                + ", tongTNVThamGia=" + tongTNVThamGia
+                + '}';
+    }
+
 }
